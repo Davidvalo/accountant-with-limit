9142063e3eeee2929635d52c622a33f0fa61920c
Davidvalo <davidclashrc@gmail.com>
2022-11-20 22:41:10 -0300

    "initial character count "

--- src/TextCounter.js
+++ src/TextCounter.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 
 class TextCounter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            totalChars: 0
+        }
+
+        
+    }
+
+
     render() {
+        const {state} = this;
+
         return ( <div>
             <h1>Meu Contador</h1>
             <textarea />
             <div>
-                <strong>Total:</strong>
+                <strong>Total:</strong>{state.totalChars}
             </div>
         </div>
         )
